ff91971ebb72716f7fa4abc0ceafaafe03104ed4
Christoph Spörk <christoph.spoerk@gmail.com>
2023-01-05 00:22:53 +0100

    chore(cleanup): Adding pylint, cleaning up found lint issues, add docstrings

--- manage/update_manifest.py
+++ manage/update_manifest.py
@@ -11,15 +11,16 @@ def update_manifest():
         if value in ["--version", "-V"]:
             version = sys.argv[index + 1]
 
-    with open(f"{os.getcwd()}/custom_components/wnsm/manifest.json") as manifestfile:
+    with open(
+        f"{os.getcwd()}/custom_components/wnsm/manifest.json", encoding="utf-8"
+    ) as manifestfile:
         manifest = json.load(manifestfile)
 
     manifest["version"] = version
 
     with open(
-        f"{os.getcwd()}/custom_components/wnsm/manifest.json", "w"
+        f"{os.getcwd()}/custom_components/wnsm/manifest.json", "w", encoding="utf-8"
     ) as manifestfile:
         manifestfile.write(json.dumps(manifest, indent=4, sort_keys=True))
-
 
 update_manifest()
