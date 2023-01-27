6dbecda4784aad1ac4d9b45a6cb321908921da07
Sebastian Bachmann <bachmann@ilsb.tuwien.ac.at>
2023-01-28 00:28:54 +0100

    subtract the right stuff

--- custom_components/wnsm/sensor.py
+++ custom_components/wnsm/sensor.py
@@ -330,7 +330,7 @@ class SmartmeterSensor(SensorEntity):
             # Extra check to not strain the API too much:
             # If the last insert date is less than 24h away, simply exit here, 
             # because we will not get any data from the API
-            delta_t = start.replace(microsecond=0) - today(timezone.utc).replace(microsecond=0)
+            delta_t = today(timezone.utc).replace(microsecond=0) - start.replace(microsecond=0)
             if delta_t <= timedelta(hours=24):
                 _LOGGER.debug(f"Not querying the API, because last update is not older than 24 hours. Earliest update in {delta_t}")
                 return
