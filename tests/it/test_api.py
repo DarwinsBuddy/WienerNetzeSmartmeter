a89ef7eb312b310e4b0d571cf7a9da826c90550d
Christoph Spörk <christoph.spoerk@gmail.com>
2023-02-26 12:08:47 +0100

    chore(test): Restructure fixtures

--- tests/it/test_api.py
+++ tests/it/test_api.py
@@ -1,27 +1,25 @@
 """API tests"""
 import pytest
-from requests_mock.mocker import Mocker
+from requests_mock import Mocker
 
-from test_resources import expect_login, USERNAME, PASSWORD, expect_zaehlpunkte
-from wnsm import api
-
-
-@pytest.fixture
-def smartmeter(username=USERNAME, password=PASSWORD):
-    return api.client.Smartmeter(username=username, password=password)
+from it import expect_login, smartmeter, expect_zaehlpunkte, zaehlpunkt, enabled, disabled
 
 
-def test_login(smartmeter, requests_mock: Mocker):
+@pytest.mark.usefixtures("requests_mock")
+def test_login(requests_mock: Mocker):
     expect_login(requests_mock)
-    smartmeter.login()
+
+    smartmeter().login()
     assert True
 
 
-def test_zaehlpunkte(smartmeter, requests_mock):
+@pytest.mark.usefixtures("requests_mock")
+def test_zaehlpunkte(requests_mock: Mocker):
     expect_login(requests_mock)
-    smartmeter.login()
-    expect_zaehlpunkte(requests_mock)
-    zps = smartmeter.zaehlpunkte()
+    expect_zaehlpunkte(requests_mock, [enabled(zaehlpunkt()), disabled(zaehlpunkt())])
+
+    smartmeter().login()
+    zps = smartmeter().zaehlpunkte()
     assert 2 == len(zps[0]['zaehlpunkte'])
     assert zps[0]['zaehlpunkte'][0]['isActive']
     assert not zps[0]['zaehlpunkte'][1]['isActive']
